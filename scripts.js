3cc7aaae7e07973a37b12f0e69d8f125051d0e44
Adelle Pitsas <apitsas4@gmail.com>
2022-09-15 20:25:44 -0400

    query select relevant elements for lock function

--- scripts.js
+++ scripts.js
@@ -2,13 +2,27 @@ var newPaletteButton = document.querySelector('.buttons_new-palette')
 var saveButton = document.querySelector('.buttons_save-palette');
 var miniSavedPalettes = document.querySelector('.mini_palette');
 var currentPaletteMini = document.querySelector('.mini');
+var color0 = document.querySelector('#color0');
+var color1 = document.querySelector('#color1');
+var color2 = document.querySelector('#color2');
+var color3 = document.querySelector('#color3');
+var color4 = document.querySelector('#color4');
+//Everything below is the parent of the color boxes above
+var colorSection0 = document.querySelector('color-section1');
+var colorSection1 = document.querySelector('color-section2');
+var colorSection2 = document.querySelector('color-section3');
+var colorSection3 = document.querySelector('color-section4');
+var colorSection4 = document.querySelector('color-section5');
+
 // Golbal Variables
 var mainPalette = new Palette();
 var savedPalettes = [];
 
+//Event Listener
 window.addEventListener('load', generateNewPalette);
 newPaletteButton.addEventListener('click', generateNewPalette);
 saveButton.addEventListener('click', savePalette);
+palette0.addEventListener('click', )
 
 function generateNewPalette() {
   mainPalette.replaceColor()
